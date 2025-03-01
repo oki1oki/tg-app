bfd72f5627d3aa9af686aa4a2de82555b01d26e1
Антон Михайлов <antonmih006@gmail.com>
2025-03-01 10:45:47 +0300

    fix

--- src/components/user.tsx
+++ src/components/user.tsx
@@ -2,10 +2,15 @@
 
 export function User() {
 	if (typeof window === "undefined") return null;
-	const tg = window.Telegram?.WebApp;
-	// const user = tg?.initDataUnsafe.user;
+	const tg = window.Telegram.WebApp;
+	const user = tg.initDataUnsafe.user;
 
-	// if (!user) return null;
+	if (!user) return null;
 
-	return <div className="">{JSON.stringify(tg)}</div>;
+	return (
+		<>
+			<div className="">{JSON.stringify(tg)}</div>
+			<div className="">{JSON.stringify(user)}</div>
+		</>
+	);
 }
