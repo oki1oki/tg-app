87c949b4642bf530fab016a5ce8e6a0dd79e9e3b
Антон Михайлов <antonmih006@gmail.com>
2025-03-01 10:29:09 +0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,5 @@
-"use client";
-
-import Image from "next/image";
+import { User } from "@/components/user";
 
 export default function Home() {
-	const tg = window.Telegram.WebApp;
-	const user = tg.initDataUnsafe.user;
-
-	if (!user) return null;
-
-	return (
-		<div className="">
-			{user.id}
-			{user?.first_name}
-			<Image
-				src={user.photo_url || ""}
-				alt="img"
-				width={400}
-				height={600}
-			/>
-		</div>
-	);
+	return <User />;
 }
---
+++ src/components/user.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import Image from "next/image";
+
+export function User() {
+	if (typeof window === "undefined") return null;
+	const tg = window.Telegram.WebApp;
+	const user = tg.initDataUnsafe.user;
+
+	if (!user) return null;
+
+	return (
+		<div className="">
+			{user.id}
+			{user?.first_name}
+			<Image
+				src={user.photo_url || ""}
+				alt="img"
+				width={400}
+				height={600}
+			/>
+		</div>
+	);
+}
