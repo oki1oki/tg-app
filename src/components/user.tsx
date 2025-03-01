87cbebaac0dfc3626cf0f14f14e491a3749b667d
Антон Михайлов <antonmih006@gmail.com>
2025-03-01 10:39:17 +0300

    tg

--- src/components/user.tsx
+++ src/components/user.tsx
@@ -1,24 +1,11 @@
 "use client";
 
-import Image from "next/image";
-
 export function User() {
 	if (typeof window === "undefined") return null;
 	const tg = window.Telegram?.WebApp;
-	const user = tg?.initDataUnsafe.user;
+	// const user = tg?.initDataUnsafe.user;
 
-	if (!user) return null;
+	// if (!user) return null;
 
-	return (
-		<div className="">
-			{user.id}
-			{user?.first_name}
-			<Image
-				src={user.photo_url || ""}
-				alt="img"
-				width={400}
-				height={600}
-			/>
-		</div>
-	);
+	return <div className="">{JSON.stringify(tg)}</div>;
 }
